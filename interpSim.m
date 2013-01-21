e572e62bf6276a5c05c576aed84956c73005e703
sgess <spencergessner@gmail.com>
2013-01-21 11:11:02 -0800

    added interpolation

---
+++ interpSim.m
@@ -0,0 +1,21 @@
+function SIM_SPEC = interpSim(OUT,spectrum_axis)
+
+% Identify Max and Min of Simulated energy distribution
+x_max = OUT.X.AXIS(256);
+x_min = OUT.X.AXIS(1);
+
+% Find the Max and Min on the YAG energy axis
+[~,iMax] = min(abs(x_max - spectrum_axis));
+[~,iMin] = min(abs(x_min - spectrum_axis));
+N = iMax - iMin + 1;
+
+% Interpolate the simulated distribution onto the YAG axis
+xx = linspace(1,256,N);
+SX = interp1(OUT.X.HIST,xx);
+sim_sum = sum(SX);
+sim_cen = round(sum((1:N).*SX)/sim_sum);
+
+% Embed onto spectrum axis
+PIX = length(spectrum_axis);
+SIM_SPEC = zeros(PIX,1);
+SIM_SPEC(round(PIX/2-sim_cen):round(PIX/2-sim_cen+N-1)) = SX/sim_sum;
--- run_LiTrackOpt.m
+++ run_LiTrackOpt.m
@@ -1,13 +1,29 @@
 clear all;
 
+% Load sample spectra
+load('data_samples.mat');
+data_spectrum = SPECTRA(:,1)/sum(SPECTRA(:,1));
+
 % Create Parameter struct
 global PARAM;
 
 % Set Paramete guess values
 sim_params;
 
-%Run LiTrack
+% Run LiTrack
 OUT = LiTrackOpt('FACETpar');
 
-%Plot Output
-plot(OUT.X.AXIS,OUT.X.HIST);
+% Interpolate simulated spectrum 
+sim_spectrum = interpSim(OUT,spectrum_axis);
+
+residual = sum((sim_spectrum - data_spectrum).^2);
+
+% Plot Output
+%plot(spectrum_axis,sim_spectrum,'color','b');
+%hold on;
+%plot(spectrum_axis,data_spectrum,'color','g');
+plot(spectrum_axis,data_spectrum,'g',spectrum_axis,sim_spectrum,'b');
+legend('DATA','SIMULATION','fontsize',14);
+xlabel('X (mm)','fontsize',14);
+text(-3.5,5e-3,['Residual = ' num2str(residual,'%0.2e')],'fontsize',14);
+
