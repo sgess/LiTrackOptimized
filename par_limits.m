6c74e8db6a57e891ad49cc6b68c56b1cf99cd440
sgess <spencergessner@gmail.com>
2013-01-21 16:16:56 -0800

    parameter limits

---
+++ par_limits.m
@@ -0,0 +1,55 @@
+par_name = {'Initial bunch length';
+            'Initial energy spread';
+            'Initial number of particles';
+            'Initial asymmetry';
+            'NRTL Compressor Amplitude';
+            'NRTL Compressor Phase';
+            'NRTL Low-energy cutoff';
+            'NRTL High-energy cutoff';
+            'Decker Phase';
+            'Ramp Phase';
+            'S10 Low-energy cutoff';
+            'S10 High-energy cutoff';
+            'S20 Low-energy cutoff';
+            'S20 High-energy cutoff';
+            'S20 Beta';
+            'YAG Dispersion';
+            'YAG 2nd Order Dispersion';};
+
+% Lower limit on params    
+Low_limit = [5e-3;
+             4e-3;
+             1.8e10;
+             -0.3;
+             0.03;
+             87;
+             -0.04;
+             0.02;
+             -25;
+             -5;
+             -0.05;
+             0.025;
+             -0.04;
+             0.02;
+             2;
+             90;
+             0;];
+         
+% Higher limit on params                 
+High_limit= [10e-3;
+             11e-3;
+             2.4e10;
+             0.0;
+             0.045;
+             93;
+             -0.02;
+             0.04;
+             -15;
+             +5;
+             -0.025;
+             0.05;
+             -0.02;
+             0.04;
+             6;
+             130;
+             2000;];
--- run_LiTrackOpt.m
+++ run_LiTrackOpt.m
@@ -1,5 +1,7 @@
 clear all;
 
+fontsize = 14;
+
 % Load sample spectra
 load('data_samples.mat');
 data_spectrum = SPECTRA(:,1)/sum(SPECTRA(:,1));
@@ -7,7 +9,10 @@ data_spectrum = SPECTRA(:,1)/sum(SPECTRA(:,1));
 % Create Parameter struct
 global PARAM;
 
-% Set Paramete guess values
+% Parameter limit file
+par_limits;
+
+% Set Parameter guess values
 sim_params;
 
 % Run LiTrack
@@ -16,14 +21,11 @@ OUT = LiTrackOpt('FACETpar');
 % Interpolate simulated spectrum 
 sim_spectrum = interpSim(OUT,spectrum_axis);
 
+% Calculate residual
 residual = sum((sim_spectrum - data_spectrum).^2);
 
 % Plot Output
-%plot(spectrum_axis,sim_spectrum,'color','b');
-%hold on;
-%plot(spectrum_axis,data_spectrum,'color','g');
 plot(spectrum_axis,data_spectrum,'g',spectrum_axis,sim_spectrum,'b');
-legend('DATA','SIMULATION','fontsize',14);
+legend('DATA','SIMULATION');
 xlabel('X (mm)','fontsize',14);
 text(-3.5,5e-3,['Residual = ' num2str(residual,'%0.2e')],'fontsize',14);
-
