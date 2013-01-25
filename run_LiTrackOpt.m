bf065294657e89102bb986a342a1be7045bfb7bb
sgess <spencergessner@gmail.com>
2013-01-24 20:33:42 -0800

    ran on my mac

--- run_LiTrackOpt.m
+++ run_LiTrackOpt.m
@@ -36,7 +36,7 @@ dt=(2*pi)/(20*w(18));
 
 
 % Total Number of Extremum Seeking Steps
-ESsteps = 4000;
+ESsteps = 2000;
 
 % ES Time, a purely digital entity
 EST = ESsteps*dt;
@@ -101,7 +101,7 @@ for j=1:ESsteps-1;
     
     % Set Cost as the value of the residual
     %cost(j) = residual;
-    cost(j) = 14 + log(residual(j)) + 0.5*Part_frac(j);
+    cost(j) = 14 + log(residual(j)) + 0.01*Part_frac(j);
     
     pscaled(:,j)=2*(params(:,j)-Cent)./Diff;
     
