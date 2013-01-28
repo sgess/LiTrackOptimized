856be07e92357c0da8184ac985ea90d95271025c
sgess <spencergessner@gmail.com>
2013-01-28 00:50:34 -0800

    updated par limits

---
+++ new_par_lims.m
@@ -0,0 +1,61 @@
+par_name = {'Initial bunch length';
+            'Initial energy spread';
+            'Initial number of particles';
+            'Initial asymmetry';
+            'NRTL Compressor Amplitude';
+            'NRTL Compressor Phase';
+            'NRTL Low-energy cutoff';
+            'NRTL High-energy cutoff';
+            'Decker Phase';
+            'Ramp Phase';
+            'S10 Low-energy cutoff';
+            'S10 High-energy cutoff';
+            'S20 Low-energy cutoff';
+            'S20 High-energy cutoff';
+            'S20 Beta';
+            'YAG Dispersion';
+            'YAG 2nd Order Dispersion';
+            'Energy Offset';};
+
+% Lower limit on params    
+Low_limit = [4e-3;
+             4e-4;
+             1.8e10;
+             -0.3;
+             0.02;
+             83;
+             -0.04;
+             0.02;
+             -25;
+             -5;
+             -0.07;
+             0.025;
+             -0.04;
+             0.02;
+             2;
+             90;
+             -300;
+             -0.01;];
+         
+% Higher limit on params                 
+High_limit= [10e-3;
+             12e-4;
+             2.6e10;
+             0.1;
+             0.045;
+             93;
+             -0.02;
+             0.04;
+             -15;
+             +5;
+             -0.025;
+             0.05;
+             -0.02;
+             0.04;
+             8;
+             150;
+             2000;
+             0.01;];
+         
+Cent=(High_limit+Low_limit)/2;
+Diff=High_limit-Low_limit;
