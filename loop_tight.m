d2c347b1fdd0a6e578a7c25a5c583cf48d93713a
sgess <spencergessner@gmail.com>
2013-06-06 00:11:20 -0700

    asfd

--- loop_tight.m
+++ loop_tight.m
@@ -2,49 +2,107 @@ clear all;
 
 file_name = '~/Desktop/scan.mat';
 savE = 1;
-scan = 1;
-test = 0;
-global PARAM;
-param_04_16_13;
-PARAM.LI20.R16   = 85;
-PARAM.LI20.BETA  = 3.0;
-PARAM.LI20.T166  = 0;
-PARAM.NRTL.R56 = 0.603;
-PARAM.NRTL.T566 = 1.3;
+n_out = 3;
+n_interp = 513;
 
 global A;
 A = load('slac.dat');
 
-%sigz0 range
-sigmaz_lo = 0.0055;
-sigmaz_hi = 0.007;
+global PARAM;
+param_04_16_13;
 
-% particle range
-npart_lo = 2.0e10;
-npart_hi = 2.2e10;
+disp_lo = 80;
+deck_lo = -21.5;
+part_lo = 1.90E10;
+sigz_lo = 0.0058;
+ampl_lo = 0.0395;
+phas_lo = 89.9;
 
-% comp phase range
-nrtlPhas_lo = 89.7;
-nrtlPhas_hi = 90.7;
+disp_hi = 100;
+deck_hi = -19.5;
+part_hi = 2.30E10;
+sigz_hi = 0.0074;
+ampl_hi = 0.0405;
+phas_hi = 90.5;
 
-% comp phase range
-nrtlAmpl_lo = 0.0405;
-nrtlAmpl_hi = 0.0415;
+disp_el = 5;
+deck_el = 5;
+part_el = 5;
+sigz_el = 5;
+ampl_el = 5;
+phas_el = 5;
 
-%2-10 range
-phasOne_lo = -22;
-phasOne_hi = -20.5;
+disps = linspace(disp_lo,disp_hi,disp_el);
+decks = linspace(deck_lo,deck_hi,deck_el);
+parts = linspace(part_lo,part_hi,part_el);
+sigzs = linspace(sigz_lo,sigz_hi,sigz_el);
+ampls = linspace(ampl_lo,ampl_hi,ampl_el);
+phass = linspace(phas_lo,phas_hi,phas_el);
 
-%11-20 phase
-phasTwo_lo = -8;
-phasTwo_hi = -4;
+inds = zeros(disp_el,deck_el,part_el,sigz_el,ampl_el,phas_el);
+nsim = numel(inds);
+r_16 = zeros(nsim,1);
+deck = zeros(nsim,1);
+part = zeros(nsim,1);
+sigz = zeros(nsim,1);
+ampl = zeros(nsim,1);
+phas = zeros(nsim,1);
 
-% number of sample points
-sigmaz_el   = 9;
-npart_el    = 9;
-nrtlPhas_el = 9;
-nrtlAmpl_el = 9;
-phasOne_el  = 9;
-phasTwo_el  = 9;
+I_max = zeros(nsim,1);
+N_par = zeros(nsim,1);
+
+zz = linspace(-0.4,0.4,n_interp);
+ee = linspace(-4,4,n_interp);
+xx = linspace(-4,4,n_interp);
+
+bl      = zeros(nsim,n_interp);
+es      = zeros(nsim,n_interp);
+sy      = zeros(nsim,n_interp);
+
+for i = 1:disp_el
+    for j = 1:deck_el
+        for k = 1:part_el
+            for l = 1:sigz_el
+                for m = 1:ampl_el
+                    for n = 1:phas_el
+                        
+                        ind = sub2ind(size(inds),n,l,m,k,j,i);
+                        disp(['Percent Complete: ' num2str(ind/nsim,'%0.2f')]);
+                        
+                        PARAM.LI20.R16   = disps(i);
+                        PARAM.LONE.DECK  = decks(j);
+                        PARAM.INIT.NPART = parts(k);
+                        PARAM.INIT.SIGZ0 = sigzs(l);
+                        PARAM.NRTL.AMPL  = ampls(m);
+                        PARAM.NRTL.PHAS  = phass(n);
+                        
+                        r_16(ind) = disps(i);
+                        deck(ind) = decks(j);
+                        part(ind) = parts(k);
+                        sigz(ind) = sigzs(l);
+                        ampl(ind) = ampls(m);
+                        phas(ind) = phass(n);
+                        
+                        PARAM.LONE.PHAS = PARAM.LONE.RAMP+PARAM.LONE.DECK;
+                        PARAM.LTWO.PHAS = PARAM.LONE.RAMP;
+                        PARAM.LONE.GAIN = (PARAM.ENRG.E1 - PARAM.ENRG.E0)/cosd(PARAM.LONE.PHAS);
+                        PARAM.LTWO.GAIN = (PARAM.ENRG.E2 - PARAM.ENRG.E1)/cosd(PARAM.LTWO.PHAS);
+                        
+                        OUT = LiTrackOpt('FACETpar');
+                        
+                        I_max(ind) = OUT.I.PEAK(n_out);
+                        N_par(ind) = OUT.I.PART(n_out);
+                        
+                        bl(ind,:) = interp1(OUT.Z.AXIS(:,n_out)-mean(OUT.Z.AXIS(:,n_out)),OUT.Z.HIST(:,n_out),zz,'linear',0);
+                        es(ind,:) = interp1(OUT.E.AXIS(:,n_out)-mean(OUT.E.AXIS(:,n_out)),OUT.E.HIST(:,n_out),ee,'linear',0);
+                        sy(ind,:) = interp1(OUT.X.AXIS-mean(OUT.X.AXIS),OUT.X.HIST,xx,'linear',0);
+                        
+                        
+                    end
+                end
+            end
+        end
+    end
+end
 
 
